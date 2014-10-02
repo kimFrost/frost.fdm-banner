0ecd5b5d6ea63980666a889ff2d3977048d4c7ab
Kim Frost <kim@noerd.dk>
2014-10-02 13:12:16 +0200

    (added) - init commit

---
+++ assets/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fdm-banner - MainController
+ * @description
+ */
+ 
+angular.module('fdm-banner')
+  .controller('MainController', function ($scope) {
+    console.log('Main Controller Init()');
+  });
